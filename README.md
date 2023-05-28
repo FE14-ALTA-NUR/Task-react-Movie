50e8dc04041b7d607d4e7a5d5fb0047913fbaf2b
Nur Cahyanto <nur.razdan@gmail.com>
2023-05-28 21:07:28 +0700

    update fetch api TMDB

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
  tugas release 1 sudah selesai dikerjakan dan file pengerjaan ada di branch main
 
  # Release 2 (Fetch API)
-
+tugas release 2 sudah berhasil fatch api dari TMDB . source codenya ada di branch release2
  # Release 3 (Detail Page)
 
  # Release 4 (Favorite Page)
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,20 +1,64 @@
 import React from 'react';
-import MovieItem from './MovieItem';
+import axios, { AxiosResponse } from 'axios';
 
-class MovieList extends React.Component {
+import MovieItem from './MovieItem';
+interface Movie {
+  id: number;
+  title: string;
+  vote_average: number;
+  release_date: string;
+  overview: string;
+  poster_path: string | null;
+}
+
+interface MovieListState {
+  movies: Movie[];
+}
+
+class MovieList extends React.Component<{}, MovieListState> {
+  state: MovieListState = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    this.fetchNowPlayingMovies();
+  }
+
+  fetchNowPlayingMovies = () => {
+    const API_KEY = 'a37dc4ea97f3cd0d8bad3a9b9022cf56';
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
+
+    axios
+      .get(url)
+      .then((response: AxiosResponse) => {
+        this.setState({ movies: response.data.results });
+      })
+      .catch((error) => {
+        console.error('Error fetching now playing movies:', error);
+      });
+  };
+
   render() {
-    // Sample movie data
-    const movies = [
-      { id: 1, title: 'ini adalah contoh judul film paling laris', rating: 7.5, img: 'https://cdn1-production-images-kly.akamaized.net/1uaACs5LpFTiCgDRScrNcBPmvLk=/375x500/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/4403395/original/057153600_1682043901-MV5BNzZmOTU1ZTEtYzVhNi00NzQxLWI5ZjAtNWNhNjEwY2E3YmZjXkEyXkFqcGdeQXVyODk4OTc3MTY_._V1_FMjpg_UX1000_.jpg' },
-      { id: 2, title: 'ini adalah contoh judul film paling laris', rating: 7.5, img: 'https://cdn1-production-images-kly.akamaized.net/1uaACs5LpFTiCgDRScrNcBPmvLk=/375x500/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/4403395/original/057153600_1682043901-MV5BNzZmOTU1ZTEtYzVhNi00NzQxLWI5ZjAtNWNhNjEwY2E3YmZjXkEyXkFqcGdeQXVyODk4OTc3MTY_._V1_FMjpg_UX1000_.jpg' },
-      { id: 3, title: 'ini adalah contoh judul film paling laris', rating: 7.5, img: 'https://cdn1-production-images-kly.akamaized.net/1uaACs5LpFTiCgDRScrNcBPmvLk=/375x500/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/4403395/original/057153600_1682043901-MV5BNzZmOTU1ZTEtYzVhNi00NzQxLWI5ZjAtNWNhNjEwY2E3YmZjXkEyXkFqcGdeQXVyODk4OTc3MTY_._V1_FMjpg_UX1000_.jpg' },
-     
-    ];
+    const { movies } = this.state;
 
     return (
-      <div className='m-10 flex flex-column flex-wrap'>
+      <div className="grid grid-cols-3 gap-4">
+
         {movies.map((movie) => (
           <MovieItem key={movie.id} movie={movie} />
+          // <div key={movie.id} className="card bg-base-200 p-4">
+          //   {movie.poster_path && (
+          //     <img
+          //       src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          //       alt={movie.title}
+          //       className="mx-auto mb-4"
+          //     />
+          //   )}
+          //   <h3 className="text-xl font-bold mb-2">{movie.title}</h3>
+          //   <p>Rating: {movie.vote_average}</p>
+          //   <p>Release Date: {movie.release_date}</p>
+          //   <p>Overview: {movie.overview}</p>
+          // </div>
         ))}
       </div>
     );
